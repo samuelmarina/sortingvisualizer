1f1567e1fb4bd79269b4130a46ff9b356a3bf444
Samuel Miller <samuellmiller04@gmail.com>
2019-12-14 13:38:02 -0400

    Initial Commit

---
+++ Sorting Visualizer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sorting Visualizer
+//
+//  Created by Samuel Miller on 12/14/19.
+//  Copyright © 2019 Samuel Miller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
